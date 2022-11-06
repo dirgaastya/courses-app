d2d1b4459ec13559b915c80957cfafb6815c21fb
dirgaastya <astya444@gmail.com>
2022-11-06 10:47:18 +0700

    add guarded column in models & add coloumn in user

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Form extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'no_user',
+        'period_id'
+    ];
 }
